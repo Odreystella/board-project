ffb3c4e4d36835eecbae99b00664c3c2da47a219
Oderystella <ddott59@gmail.com>
2021-10-27 13:52:06 +0900

    feat: create base.html, board_list.html

---
+++ boards/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = "boards"
+
+urlpatterns = [
+#   path("", MovieListView.as_view(), name="movies" )
+]
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from boards.models import Board
 
-# Create your views here.
+
+class BoardListView(ListView):
+
+    model = Board
+    template_name = "boards/board_list.html"
+    context_object_name = "boards"
+    ordering = "-created_at"
+    paginate_by = 10
+    paginate_orphans = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "All Articles"
+        return context
